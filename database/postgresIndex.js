416654a9cca6cf7a054ddc1f889d17177781a940
bryan lee <bryanlee@bryans-MacBook-Air.local>
2019-03-07 18:51:22 -0800

    changed host

--- database/postgresIndex.js
+++ database/postgresIndex.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('suggestions', 'ubuntu', '', {
-  host: 'localhost',
+  host: 'ec2-52-14-197-39.us-east-2.compute.amazonaws.com',
   dialect: 'postgres',
 })
 
