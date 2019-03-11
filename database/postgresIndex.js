6f0f75f9d850aa35465ce4755bcea999b4670799
bryan lee <bryanlee@bryans-MacBook-Air.local>
2019-03-11 14:48:09 -0700

    changed to pool

--- database/postgresIndex.js
+++ database/postgresIndex.js
@@ -1,4 +1,13 @@
 const Sequelize = require('sequelize');
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  host: '52.14.197.39',
+  user: 'postgres',
+  password: 'password',
+  database: 'suggestions'
+})
+
 const sequelize = new Sequelize('suggestions', 'postgres', 'password', {
   host: '52.14.197.39',
   // port:5432,
@@ -7,11 +16,14 @@ const sequelize = new Sequelize('suggestions', 'postgres', 'password', {
   operatorsAliases: false
 })
 
-// sequelize
-//   .authenticate()
-//   .then(() => console.log('Successfully connected to the databases'))
-//   .catch(err => console.log('Error establishing connection to database', err))
+sequelize
+  .authenticate()
+  .then(() => console.log('Successfully connected to the databases'))
+  .catch(err => console.log('Error establishing connection to database', err))
 
 
-  
-module.exports = sequelize;
+pool.query('select NOW()', (err, data) => {
+  console.log(err, data.rows)
+})
+
+module.exports = pool;
