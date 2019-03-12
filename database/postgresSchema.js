3d7e31a3f2880621b59b93e3a62ed9afef4254d5
bryan lee <bryanlee@bryans-MacBook-Air.local>
2019-03-12 14:19:07 -0700

    changed schema

--- database/postgresSchema.js
+++ database/postgresSchema.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = require('./postgresIndex.js');
+const {sequelize }= require('./postgresIndex.js');
 
 const Product = sequelize.define(
     'product', 
